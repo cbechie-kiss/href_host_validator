a5800c87917c8827c56596501401afbfd8737f24
cbechie-kiss <107353602+cbechie-kiss@users.noreply.github.com>
2023-06-03 13:57:24 -0700

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import urllib.request
+import requests
 from bs4 import BeautifulSoup
 import sys
 import hashlib
@@ -83,18 +84,29 @@ Hosts = HostList.__new__(HostList)
 server_ip = ""
 server_port = ""
 base_url = ""
+DEBUG = 0
 
 
-def get_web_page(domain, webpage):
-    print("Retrieving Webpage: " + domain + "/" + urllib.parse.unquote(webpage))
+def separate_location_from_host(location):
+    if (location[0:7] == "http://") or (location[0:8] == "https://"):
+        location = location.split('//', 1)[1]
+    if "/" in location:
+         location = location.split('/', 1)[1]
+    return location
 
-    req = urllib.request.Request(base_url + ":" + server_port + "/" + webpage, headers={"Host": domain, "User-Agent": USER_AGENT}, method="GET")
-    try:
-        resp = urllib.request.urlopen(req, timeout=5)
-    except Exception as exc:
-        return None
 
-    return resp
+def separate_host_from_location(location):
+    if (location[0:7] == "http://") or (location[0:8] == "https://"):
+        location = location.split('//', 1)[1]
+    if "/" in location:
+         location = location.split('/', 1)[0]
+    return location
+
+
+def is_local_ipv4_private_address():
+    # Regular expression pattern to match local IPv4 private addresses. ChatGPT
+    pattern = r'^(?:10\.|172\.(?:1[6-9]|2[0-9]|3[0-1])\.|192\.168\.)\d{1,3}\.\d{1,3}$'
+    return bool(re.match(pattern, server_ip))
 
 
 def is_hash_dup(new_host):
@@ -112,7 +124,7 @@ def is_name_dup(name):
 
 
 def is_a_email_domain(new_host):
-    if len(Hosts.hosts) != 0 :
+    if len(Hosts.hosts) != 0:
         original_host = Hosts.hosts[0]
         if original_host.hash == new_host.hash:
             for email in original_host.email_list.emails:
@@ -120,7 +132,16 @@ def is_a_email_domain(new_host):
                     return True
 
 
+def to_crawl_or_not_to_crawl(domain):
+    ans = input("Do you want to crawl " + domain + " (y/N): ").lower()
+    if (ans == "y") or (ans == "yes"):
+        return True
+    else:
+        return False
+
+
 def test_host(domain):
+    # Prompt user if they want to crawl domain
     if is_name_dup(domain) is True:
         return None
     new_host = Host.__new__(Host)
@@ -132,9 +153,20 @@ def test_host(domain):
     new_host.href_list.__init__([], 0, 0, 0)
     new_host.email_list.__init__([], 0, 0, 0)
 
-    print("Testing Host: " + domain)
-    req = urllib.request.Request(base_url + ":" + server_port, headers={"Host": domain, "User-Agent": USER_AGENT}, method="GET")
+    local = is_local_ipv4_private_address()
 
+    if local is False:
+        new_host.is_in_scope = to_crawl_or_not_to_crawl(domain)
+
+    if new_host.is_in_scope is False:
+        new_host.is_enumerated = False
+        Hosts.hosts.append(new_host)
+        Hosts.total = Hosts.total + 1
+        return
+
+    print("Testing Host: " + domain)
+    req = urllib.request.Request(base_url, headers={"Host": separate_host_from_location(domain),
+                                                    "User-Agent": USER_AGENT}, method="GET")
     try:
         #print(req.host + " " + str(req.headers))
         resp = urllib.request.urlopen(req, timeout=30)
@@ -147,7 +179,6 @@ def test_host(domain):
         return None
 
     new_host.hash = hashlib.sha256(resp.read()).hexdigest()
-
 
     if is_a_email_domain(new_host) is True:
         new_host.is_in_scope = True
@@ -166,6 +197,21 @@ def test_host(domain):
     Hosts.total = Hosts.total + 1
 
 
+def get_web_page(domain, webpage):
+
+    if (len(webpage.strip()) != 0) and (webpage[0] == "/"):
+        webpage = webpage[1:]
+
+    print("Retrieving Webpage: " + base_url + "/" + urllib.parse.unquote(webpage))
+    header = {"Host": separate_host_from_location(domain), "User-Agent": USER_AGENT}
+    if domain in host.name:
+        try:
+            resp = requests.get(base_url + "/" + webpage, headers=header)
+        except Exception as exc:
+            return None
+        return resp.content
+
+
 def accumulate(attributes_so_far, key, value):
     if not isinstance(attributes_so_far[key], list):
         attributes_so_far[key] = [attributes_so_far[key]]
@@ -182,7 +228,6 @@ def is_dup_href(host: Host, location):
 
 def is_href_offsite(host: Host, href):
     href.location = str(href.location)
-    #print(href.location)
     if len(href.location.strip()) == 0:
         href.is_offsite = False
         href.is_enumerated = True
@@ -220,23 +265,12 @@ def create_new_href(host: Host, new_href):
     host.href_list.total = host.href_list.total + 1
 
 
-def separate_location_from_host(location):
-    if (location[0:7] == "http://") or (location[0:8] == "https://"):
-        location = location.split('//', 1)[1]
-    if "/" in location:
-         location = location.split('/', 1)[1]
-    return location
-
-
-def separate_host_from_location(location):
-    if (location[0:7] == "http://") or (location[0:8] == "https://"):
-        location = location.split('//', 1)[1]
-    if "/" in location:
-         location = location.split('/', 1)[0]
-    return location
-
-
 def driver_create_href(host: Host, location):
+
+    if location[0:8] == "mailto:":
+        return
+    if DEBUG == 1:
+        print("driver_create_href:" + str(location))
     if host.name in location:
         location = separate_location_from_host(location)
     if is_dup_href(host, str(location)) is False:
@@ -244,27 +278,32 @@ def driver_create_href(host: Host, location):
 
 
 def parse_for_href(host: Host, soup):
+    #print(soup.prettify())
     for anchor in soup.find_all('a'):
         hrefs = anchor.get('href')
 
-        if type(hrefs) == list:
-            for location in hrefs:
-                driver_create_href(host, location)
-        else:
-            driver_create_href(host, hrefs)
+        #print("PARSE_FOR_HREFS:" + str(hrefs))
+        if hrefs is not None:
+            if type(hrefs) == list:
+                for location in hrefs:
+                    driver_create_href(host, location)
+            else:
+                driver_create_href(host, hrefs)
 
 
-def enumerate_hrefs(host: Host, soup):
+def enumerate_hrefs(host: Host):
     is_all_enumerated = False
     while is_all_enumerated is False:
         for href in host.href_list.hrefs:
             if (href.is_offsite is False) and (href.is_enumerated is False):
                 resp = get_web_page(host.name, href.location)
+                soup = BeautifulSoup(resp, 'html.parser', on_duplicate_attribute=accumulate)
                 if resp is not None:
                     parse_for_href(host, soup)
                 href.is_enumerated = True
                 host.href_list.num_enumerated = host.href_list.num_enumerated + 1
-                #print(str(host.href_list.num_enumerated) + " "  + str(host.href_list.total_on_site))
+                if DEBUG == 1:
+                    print(str(host.href_list.num_enumerated) + " " + str(host.href_list.total_on_site))
         if host.href_list.total_on_site == host.href_list.num_enumerated:
             is_all_enumerated = True
 
@@ -272,7 +311,8 @@ def enumerate_hrefs(host: Host, soup):
 def test_href_for_new_site(host: Host):
     for href in host.href_list.hrefs:
         if(href.is_offsite is True) and (href.is_enumerated is False):
-            #print(href.location)
+            if DEBUG == 1:
+                print(href.location)
             href.location = separate_host_from_location(href.location)
             href.is_enumerated = True
             test_host(href.location)
@@ -281,7 +321,8 @@ def test_href_for_new_site(host: Host):
 def test_email_for_new_site(host: Host):
     for email in host.email_list.emails:
         if(email.is_offsite is True) and (email.is_enumerated is False):
-            #print(href.location)
+            if DEBUG == 1:
+                print("TEST EMAIL:" + email.domain)
             email.is_enumerated = True
             test_host(email.domain)
             host.email_list.num_enumerated = host.email_list.num_enumerated + 1
@@ -301,6 +342,22 @@ def is_email_dup(host: Host, email):
     return False
 
 
+def extract_emails(text):
+    # ChatGPT generated
+    pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    emails = re.findall(pattern, text, re.IGNORECASE)
+
+    unique_emails = list(set(emails))
+    return unique_emails
+
+
+def extract_emails_from_html(html):
+    # ChatGPT generated
+    clean_html = re.sub('<[^<]+?>', '', html)
+    emails = extract_emails(clean_html)
+    return emails
+
+
 def create_new_email(host: Host, email):
     if is_email_dup(host, email) is False:
         new_email = Email.__new__(Email)
@@ -315,27 +372,32 @@ def create_new_email(host: Host, email):
 
 
 def parse_for_emails(host: Host, soup):
-    extract_email_pattern = r"\S+@\S+\.\S+"
-    emails = re.findall(extract_email_pattern, soup.prettify())
+
+    emails = extract_emails_from_html(soup.prettify())
+
     if emails:
         for email in emails:
+            if DEBUG == 1:
+                print("EMAIL FOUND: " + str(email))
             create_new_email(host, email)
 
 
 def enumerate_site(host: Host):
     resp = get_web_page(host.name, "")
-    soup = BeautifulSoup(resp.read(), 'html.parser', on_duplicate_attribute=accumulate)
+    soup = BeautifulSoup(resp, 'html.parser', on_duplicate_attribute=accumulate)
     parse_for_href(host, soup)
     parse_for_emails(host, soup)
-
-    #  print("Enum Hrefs")
-    enumerate_hrefs(host, soup)
-    #  print("Enum Emails")
+    if DEBUG == 1:
+        print("Enum Hrefs")
+    enumerate_hrefs(host)
+    if DEBUG == 1:
+        print("Enum Emails")
     test_host_for_new_sites(host)
 
     host.is_enumerated = True
     Hosts.num_enumerated = Hosts.num_enumerated + 1
-    #  print("Enum Finished")
+    if DEBUG == 1:
+        print("Enum Finished")
 
 
 def print_emails(host: Host):
@@ -350,10 +412,12 @@ def print_emails(host: Host):
 def print_host():
     for host in Hosts.hosts:
         print("===========================================")
-        print(host.name)
-        print(host.hash)
+        print("Name: " + host.name)
+        print("Hash: " + host.hash)
         for href in host.href_list.hrefs:
-            print(urllib.parse.unquote(href.location))
+            if href is not None:
+                if (href.location[0] == '/') or (href.location[0] == "#"):
+                    print(urllib.parse.unquote(href.location))
         print("isEnumerated: ", host.is_enumerated)
         print("isInScope: ", host.is_in_scope)
         print_emails(host)
@@ -364,7 +428,7 @@ def print_new_hosts_file_entries():
     host_file_edits = []
 
     for this_host in Hosts.hosts:
-        if (this_host.is_in_scope is True) and (this_host.name != server_ip):
+        if (this_host.is_in_scope is True) and (this_host.name != server_ip) and (this_host.name != base_url):
             host_file_edits.append(server_ip + "    " + this_host.name)
 
     if len(host_file_edits) != 0:
@@ -378,11 +442,14 @@ def print_new_hosts_file_entries():
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: href_parser.py <ip> <port> <base_url>")
+        exit(-1)
     Hosts.__init__([], 0, 0, 0)
     server_ip = sys.argv[1]
     server_port = sys.argv[2]
     base_url = sys.argv[3]
-    test_host(server_ip)
+    is_local_ipv4_private_address()
+
+    test_host(base_url)
 
     is_all_enumerated = False
     while is_all_enumerated is False:
@@ -396,5 +463,4 @@ if __name__ == "__main__":
 
     print_host()
     print_new_hosts_file_entries()
-
 
